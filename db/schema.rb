df0868e6b8f0fc618fdb9ff7e5420f2303f56f06
jrsahuquillo <jrsahuquillo@gmail.com>
2016-12-05 12:17:24 +0100

    Added preferences and birthday migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205104431) do
+ActiveRecord::Schema.define(version: 20161205110445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161205104431) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "preferences"
+    t.datetime "birthday"
   end
 
 end
