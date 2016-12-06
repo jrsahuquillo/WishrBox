e1b01dd45049e1490dd5a86fd48bc939dce89b95
jrsahuquillo <jrsahuquillo@gmail.com>
2016-12-06 13:45:30 +0100

    Wish new/create methods and views

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -4,4 +4,28 @@ class WishesController < ApplicationController
       @user = User.find(params[:user_id])
       @wishes = @user.wishes
     end
+
+    def new
+      @user = User.find(params[:user_id])
+      @wish = @user.wishes.new
+    end
+
+    def create
+      @user = User.find(params[:user_id])
+        @wish = @user.wishes.new(wish_params)
+
+        if @wish.save
+          flash[:notice] = "Deseo añadido a tu WishrBox"
+          redirect_to user_wishes_path(@user)
+        else
+          render "new"
+        end
+    end
+
+private
+
+  def wish_params
+    params.require(:wish).permit(:title, :description, :link, :price)
+  end
+
 end
