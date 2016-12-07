8e2f835f534d05d58c169477a1a70c0d52e26093
jrsahuquillo <jrsahuquillo@gmail.com>
2016-12-07 18:33:00 +0100

    Configured amazon bucket out of USA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,8 @@ Rails.application.configure do
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
   }
+    s3_host_name: "s3-eu-west-1.amazonaws.com", # Added entry
+    url: ":s3_host_name"                        # Added entry
+  }
 }
 end
