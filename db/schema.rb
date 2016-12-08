4e13f52e3876f58d27418cb870af70f35db8f1a0
jrsahuquillo <jrsahuquillo@gmail.com>
2016-12-08 19:02:06 +0100

    Friendship model generate OK

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,11 @@ ActiveRecord::Schema.define(version: 20161208173412) do
   enable_extension "plpgsql"
 
   create_table "friendships", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.boolean  "approved",   default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
