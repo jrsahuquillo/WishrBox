5542457898c821801eb702e0199169b674349eb7
jrsahuquillo <jrsahuquillo@gmail.com>
2016-12-09 17:48:33 +0100

    Followed delete wishr button fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'profile', to: 'users#profile'
-  root to: 'wishes#index'
+  root to: 'users#feed'
 
 
   get '/feed', to: 'wishes#index'
   get '/search_following', to: 'users#search_following'
+  get '/', to: 'users#show'
     # post '/follow_to', to: 'users#follow_to', as: 'follow'
     # match '/follow_to',    to: 'users#follow_to', as: 'follow',   via: 'get'
 
