4da8dd942be441b2c69c242f28d038f79eabf30b
jrsahuquillo <jrsahuquillo@gmail.com>
2016-12-12 19:03:44 +0100

    Sign up fixes

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,7 +2,6 @@ class WelcomeMailer < ApplicationMailer
   default from: "wishrbox@gmail.com"
   def welcome_email(user)
     @user = user
-    binding.pry
     mail(to: user.email, subject: "Hola #{user.name}, bienvenido a WishrBox")
   end
 end
