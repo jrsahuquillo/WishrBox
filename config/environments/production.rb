c566b137aa36a30e5c623eafea90faa06d322a87
jrsahuquillo <jrsahuquillo@gmail.com>
2016-12-14 10:18:14 +0100

    Production sign up debugging10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings   = {
     	address:  "smtp.gmail.com",
-    	port: 587,
+    	port: 465, #587
     	user_name: ENV['gmail_username'],
     	password: ENV['gmail_password'],
     	authentication: "plain",
