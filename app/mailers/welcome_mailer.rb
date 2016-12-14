3ea58ab45c1a879d53344b37d23ab970915b2765
jrsahuquillo <jrsahuquillo@gmail.com>
2016-12-14 10:05:50 +0100

    Production sign up debugging9

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,6 +2,6 @@ class WelcomeMailer < ApplicationMailer
   default from: "wishrbox@gmail.com"
   def welcome_email(user)
     @user = user
-    mail(to: user.email, subject: "Hola #{user.name}, bienvenido a WishrBox")
+    mail(from: "wishrbox@gmail.com", to: user.email, subject: "Hola #{user.name}, bienvenido a WishrBox")
   end
 end
